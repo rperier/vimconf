d8b73e7d5d82efd9de451cf714b3d9e3c29ddb74
Romain Perier <romain.perier@gmail.com>
2015-05-02 10:22:50 +0200

    install.sh: Add tmux.conf

--- install.sh
+++ install.sh
@@ -3,4 +3,7 @@
 test -d $HOME/.vim/autoload || mkdir $HOME/.vim/autoload
 ln -sf $HOME/.vim/pathogen/autoload/pathogen.vim $HOME/.vim/autoload/pathogen.vim
 ln -sf $HOME/.vim/vimrc $HOME/.vimrc
+ln -sf $HOME/.vim/tmux.conf $HOME/.tmux.conf
+
+echo "Remember to put TERM=xterm-256color in your ~/.bashrc"
 
