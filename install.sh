72ae5ed907e586cfe97e22a59895aee3b3ab48b3
Romain Perier <romain.perier@scle.fr>
2016-02-16 12:24:58 +0000

    Updating install.sh to include neovim path

--- install.sh
+++ install.sh
@@ -4,9 +4,10 @@ git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 ln -sf $HOME/.vim/vimrc $HOME/.vimrc
 ln -sf $HOME/.vim/tmux.conf $HOME/.tmux.conf
 ln -sf $HOME/.vim/screenrc $HOME/.screenrc
+ln -sf $HOME/.vim $HOME/.config/nvim
+ln -sf $HOME/.vim/vimrc $HOME/.config/nvim/init.vim
 vim +BundleInstall +qall
 
 echo "Remaining tasks:"
-echo " * export TERM=screen-256color in your ~/.bashrc"
 echo " * install powerline fonts (https://github.com/powerline/fonts.git)"
 
