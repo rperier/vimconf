d1f8e2acde0ba63f8a4b17b20c598c1d72bb3543
Romain Perier <romain.perier@gmail.com>
2016-12-04 18:11:59 +0100

    Add terminator config file

--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ ln -sf $HOME/.vim $HOME/.config/nvim
 ln -sf $HOME/.vim/vimrc $HOME/.config/nvim/init.vim
 test -d $HOME/.mutt || mkdir $HOME/.mutt
 ln -sf $HOME/.vim/mutt-aliases $HOME/.mutt/aliases
+test -d $HOME/.config/terminator || mkdir $HOME/.config/terminator
+ln -sf $HOME/.vim/terminator-config $HOME/.config/terminator/config 
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall
