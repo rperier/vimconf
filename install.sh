933d74ba6f4af545d32ebbfd9ed8099ec5926f94
Romain Perier <romain.perier@gmail.com>
2018-09-16 19:51:16 +0200

    Add configuration for mutt
    This includes the muttrc file and its color theme based on lapis256
    from vim

--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ ln -sf $HOME/.vim $HOME/.config/nvim
 ln -sf $HOME/.vim/vimrc $HOME/.config/nvim/init.vim
 test -d $HOME/.mutt || mkdir $HOME/.mutt
 ln -sf $HOME/.vim/mutt-aliases $HOME/.mutt/aliases
+ln -sf $HOME/.vim/muttrc $HOME/.muttrc
+ln -sf $HOME/.vim/lapis256.muttrc $HOME/.mutt/lapis256.muttrc
 test -d $HOME/.config/terminator || mkdir $HOME/.config/terminator
 ln -sf $HOME/.vim/terminator-config $HOME/.config/terminator/config 
 if [ ! -e $HOME/.config/nvim/autoload/plug.vim ]; then
